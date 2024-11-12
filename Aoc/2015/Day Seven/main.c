607a8663b8dc365e30aba3cc62fd036a2ba588d0
Abdullah Arslan <121155456+Vi314@users.noreply.github.com>
2024-11-12 11:54:29 +0300

    cant do y2015 d7 for now

--- Aoc/2015/Day Seven/main.c
+++ Aoc/2015/Day Seven/main.c
@@ -1,57 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "main.h"
 #include "../common/utils.h"
 
 //gcc main.c ./../common/utils.c -o main && ./main.exe
-int main(int argc, char const* argv[]) {
-    FILE* fptr = fopen("input.txt", "r");
-    char buffer[25];    
-    char *destWire, *token;
-    int valLeft, valRight, idx;
-    while (fgets(buffer, 25, fptr)) {
-        printf("\nSTR: %s \t", buffer);
-        token = strtok(buffer, "-> ");
-        printf("\tToken1: %s", token);
-        token = strtok(NULL, "-> ");
-        printf("\tToken3: %s", token);
-        token = strtok(NULL, "-> ");
-        printf("\tToken4: %s", token);
-        
-        destWire = strtok(buffer, "-> \n");
-        printf("\tDestWire: %s", destWire);
-
-        //idx = getIdx(destWire);
-        //Get valLeft
-
-        if (isLowerCaseNum(buffer[0])) {
-
-        } else if (isDigit(buffer[0])) {
-            
-        } else {
-            // if (strcmp(token, "AND") == 0) {
-                
-            // } else if (strcmp(token, "OR") == 0) {
-                
-            // } else if (strcmp(token, "NOT") == 0) {
-                
-            // } else if (strcmp(token, "LSHIFT") == 0) {
-
-            // } else if (strcmp(token, "RSHIFT") == 0) {
-
-            // }
-
-        }
-    }
-    return 0;
-}
-
 //AND OR NOT LSHIFT RSHIFT
 //97-122 => a-z 48-57 => 0-9
 
-int getIdx(char* chptr) {
-    if (chptr[1] == 0) {
-        chptr[1] = 1;
+int wires[1250];
+
+int main(int argc, char const* argv[]) {
+    FILE* fptr = fopen("input.txt", "r");
+    char buffer[40];
+    char* next, *token;
+    int val, secondVal, idx;
+    int indexOfa;
+    while (fgets(buffer, 25, fptr)) {
+        token = strtok(buffer, " \n");
+        while (token != NULL) {
+            if (isLowerCaseNum(token[0])) {
+                val = wires[getIdx(token)];
+            } else if (isDigit(token[0])) {
+                val = strtol(token, NULL, 10);
+            } 
+            else if (isUpperCaseNum(token[0])) {
+                next = strtok(NULL, " \n");
+                if (isLowerCaseNum(next[0])) {
+                    secondVal = wires[getIdx(next)];
+                } else if (isDigit(next[0])) {
+                    secondVal = strtol(next, NULL, 10);
+                }
+                
+                if (strcmp(token, "AND") == 0) {
+                    val = val & secondVal;
+                } else if (strcmp(token, "OR") == 0) {
+                    val = val | secondVal;
+                } else if (strcmp(token, "NOT") == 0) {
+                    val = secondVal;
+                } else if (strcmp(token, "LSHIFT") == 0) {
+                    val = val << secondVal;
+                } else if (strcmp(token, "RSHIFT") == 0) {
+                    val = val >> secondVal;
+                }
+            } else if (strcmp(token, "->") == 0) {
+                token = strtok(NULL, " \n");
+                idx = getIdx(token);
+                wires[idx] = val;
+                break;
+            }
+            token = strtok(NULL, " \n");
+        }
     }
-    return (chptr[0] % 32) * (2 * (chptr[1] % 32));
+
+    printf("Value of [a]: %d", wires[0]);
+    return 0;
+}
+
+int getIdx(char* chptr) {
+    if (chptr[1] == '\0') {
+        return chptr[0] - 'a'; // Single character wire
+    } else {
+        return (chptr[0] - 'a') * 26 + (chptr[1] - 'a'); // Double character wire
+    }
 }
